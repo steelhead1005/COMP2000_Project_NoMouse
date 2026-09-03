b6f66a7bcdbc91ff3008e267a9a24b5e5c2a4067
steelhead1005 <steelhead1005@gmail.com>
2026-09-03 14:20:49 +1000

    added abstract entity class

--- Entity.java
+++ Entity.java
@@ -1,10 +1,17 @@
-class Entity {
-    double xPos;
-    double yPos;
-    double xVelocity;
-    double yVelocity;
+public abstract class Entity {
+    protected double x, y;
+    protected double dx, dy;
 
-    Entity() {
+    public Entity(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
+    public abstract void update();
+    public abstract void render();
 
+    public double distanceTo(Entity other) {
+        double diffX = this.x - other.x;
+        double diffY = this.y - other.y;
+        return Math.sqrt(diffX * diffX + diffY * diffY);
+    }
 }
