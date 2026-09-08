e0559fd1eb8514206bf993a6f81d132b7bd56a4b
steelhead1005 <steelhead1005@gmail.com>
2026-09-08 11:09:13 +1000

    syncing?

--- BirdBehaviour.java
+++ BirdBehaviour.java
@@ -1,66 +1,69 @@
-public class BirdBehaviour implements Behaviour<Bird> {
+import java.util.List;
 
-public World update(World world) {
-    double perceptionRadius = 50.0;
-    double separationRadius = 25.0;
+public class BirdBehaviour {
 
-    double sepX = 0, sepY = 0;
-    double alignX = 0, alignY = 0;
-    double cohX = 0, cohY = 0;
+public class UBehaviour implements Behaviour<Bird> {
+    @Override
+    public void calculate(Bird self, World world) {
+            List<Bird> allBirds = world.getBirds();
+            double perceptionRadius = 50.0;
+            double separationRadius = 25.0;
 
-    int totalNeighbors = 0;
-    int sepNeighbors = 0;
+            double sepX = 0, sepY = 0;
+            double alignX = 0, alignY = 0;
+            double cohX = 0, cohY = 0;
 
-    for (Bird other : allBirds) {
-        if (other == this) continue;
+            int totalNeighbors = 0;
+            int sepNeighbors = 0;
 
-        double d = this.distanceTo(other);
+            for (Bird other : allBirds) {
+                if (other == self) continue;
 
-        if (d > 0 && d < perceptionRadius) {
-            // Alignment: sum neighbor velocities
-            alignX += other.dx;
-            alignY += other.dy;
+                double d = self.distanceTo(other);
+
+                if (d > 0 && d < perceptionRadius) {
+                    // Alignment: sum neighbor velocities
+                    alignX += other.dx;
+                    alignY += other.dy;
 
 
-            // Cohesion: sum neighbor positions
-            cohX += other.x;
-            cohY += other.y;
-            totalNeighbors++;
+                    // Cohesion: sum neighbor positions
+                    cohX += other.x;
+                    cohY += other.y;
+                    totalNeighbors++;
 
-            // Separation: sum vectors pointing away from close neighbors
-            if (d < separationRadius) {
-                double diffX = this.x - other.x;
-                double diffY = this.y - other.y;
+                    // Separation: sum vectors pointing away from close neighbors
+                    if (d < separationRadius) {
+                        double diffX = self.x - other.x;
+                        double diffY = self.y - other.y;
 
-                // Weight the separation force inversely by distance
-                sepX += (diffX / d) / d;
-                sepY += (diffY / d) / d;
-                sepNeighbors++;
+                        // Weight the separation force inversely by distance
+                        sepX += (diffX / d) / d;
+                        sepY += (diffY / d) / d;
+                        sepNeighbors++;
+                    }
+                }
             }
+
+            if (totalNeighbors > 0) {
+                // Alignment: (Average Velocity) - Current Velocity
+                alignX = (alignX / totalNeighbors) - self.dx;
+                alignY = (alignY / totalNeighbors) - self.dy;
+
+                // Cohesion: (Vector towards Average Position) - Current Velocity
+                cohX = (cohX / totalNeighbors) - self.x;
+                cohY = (cohY / totalNeighbors) - self.y;
+
+                self.applyForce(alignX * 0.05, alignY * 0.05);
+                self.applyForce(cohX * 0.005, cohY * 0.005);
+            }
+
+            if (sepNeighbors > 0) {
+                // Separation: Average escape vector
+                sepX /= sepNeighbors;
+                sepY /= sepNeighbors;
+                self.applyForce(sepX * 1.5, sepY * 1.5);
+            } 
         }
-    }
-
-    if (totalNeighbors > 0) {
-        // Alignment: (Average Velocity) - Current Velocity
-        alignX = (alignX / totalNeighbors) - this.dx;
-        alignY = (alignY / totalNeighbors) - this.dy;
-
-        // Cohesion: (Vector towards Average Position) - Current Velocity
-        cohX = (cohX / totalNeighbors) - this.x;
-        cohY = (cohY / totalNeighbors) - this.y;
-
-        this.applyForce(alignX * 0.05, alignY * 0.05);
-        this.applyForce(cohX * 0.005, cohY * 0.005);
-    }
-
-    if (sepNeighbors > 0) {
-        // Separation: Average escape vector
-        sepX /= sepNeighbors;
-        sepY /= sepNeighbors;
-        this.applyForce(sepX * 1.5, sepY * 1.5);
-    }
-
-    updatePosition();
-    limitVelocity(4.0);
     }
 }
